6f5f4fddd63619e8d73a8e34a928f953779bf660
Piotr Czarnik <piotrczarnik@student.agh.edu.pl>
2024-04-06 23:40:33 +0200

    Add cli arg to heat2d

--- examples/heat/heat_2d.cpp
+++ examples/heat/heat_2d.cpp
@@ -2,10 +2,16 @@
 // SPDX-License-Identifier: MIT
 
 #include "heat_2d.hpp"
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
+    double dt = 1e-5;
+    if (argc > 1) {
+        dt = std::stod(argv[1]);
+    }
+
     ads::dim_config dim{2, 40};
-    ads::timesteps_config steps{10000, 1e-5};
+    ads::timesteps_config steps{10000, dt};
     int ders = 1;
 
     ads::config_2d c{dim, dim, steps, ders};
