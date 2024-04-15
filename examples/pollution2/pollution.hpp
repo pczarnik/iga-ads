c229734dcf5b615acc4086a28048206f6e9735f7
Piotr Czarnik <piotrczarnik@student.agh.edu.pl>
2024-04-16 01:23:45 +0200

    Tried some things with params for pollution

--- examples/pollution2/pollution.hpp
+++ examples/pollution2/pollution.hpp
@@ -4,13 +4,14 @@
 #ifndef POLLUTION_HPP
 #define POLLUTION_HPP
 
-#include <galois/Timer.h>
 #include <iostream>
+
+#include <galois/Timer.h>
 
 #include "ads/output_manager.hpp"
 #include "ads/simulation.hpp"
 
-const int iterations = 3000; // 30000
+const int iterations = 30000;  // 30000
 namespace ads::problems {
 
 class pollution : public simulation_2d {
@@ -27,7 +28,7 @@ public:
     , u_prev{shape()}
     , output{x.B, y.B, 200} { }
 
-    double init_state(double x, double y) {
+    double init_state(double /*x*/, double /*y*/) {
         return 0;
         // double dx = x - 0.5;
         // double dy = y - 0.5;
@@ -36,7 +37,6 @@ public:
     };
 
 private:
-
     void prepare_matrices() {
         y.fix_left();
         y.fix_right();
@@ -59,12 +59,13 @@ private:
         swap(u, u_prev);
         const double d = 0.7;
         const double c = 10000;
-        s = std::max(((cos(iter * 3.14159265358979 / c) - d) * 1 / (1-d)), 0.);
-        std::cout << "\r" << iter << "/" << iterations << " (s=" << s << ")                          \r";
+        s = std::max(((cos(iter * 3.14159265358979 / c) - d) * 1 / (1 - d)), 0.);
+        std::cout << "\r" << iter << "/" << iterations << " (s=" << s
+                  << ")                          \r";
     }
 
-    void step(int /*iter*/, double t) override {
-        compute_rhs(t);
+    void step(int iter, double /*t*/) override {
+        compute_rhs(iter);
         solve(u);
     }
 
@@ -75,27 +76,47 @@ private:
     }
 
     double f(double h) {
-        if (h <= 0.125) return (150 - 1200 * h) * s;
+        if (h <= 0.125)
+            return (150 - 1200 * h) * s;
         return 0;
     }
 
-    double e2h(double e) {
-        return e / 40;
-    }
+    double e2h(double e) { return e / 40; }
 
     double dTy(double h) {
-      if (h >= 0.8) return 0;
-      return -5.2;
+        if (h >= 0.8)
+            return 0;
+        return -5.2;
     }
 
-    const double k_x = 1.0, k_y = 0.1;
-    
-    double cannon(double x, double y, double t){
-    double S = 1;
-    return S * sin(10 * 3.14159265358979 * x) * sin(10 * 3.14159265358979 * y) * std::max(0., sin(3.14159265358979 * t / 10));    
+    double max(double a, double b) { return a > b ? a : b; }
+
+    double clock(double t, int t_begin, int t_end) {
+        if (t < t_begin || t > t_end)
+            return 0;
+        t = (t - t_begin) / (t_end - t_begin);
+        return max(sin(2 * 3.14 * t) * cos(t), 0);
     }
-    
-    void compute_rhs(double t) {
+
+    double cannon(double x, double y, int iter, int t_begin, int t_end) {
+        if (x > 0.2 && x < 0.8) {
+            double t = clock(iter, t_begin, t_end);
+            if (t == 0)
+                return 0;
+            return 300 * (y - 1) * sin(3.14 * x) * t;
+        }
+        return 0;
+    }
+
+    // double cannon(double x, double y, double t) {
+    //     double S = 1;
+    //     return S * sin(10 * 3.14159265358979 * x) * sin(10 * 3.14159265358979 * y)
+    //          * std::max(0., sin(3.14159265358979 * t / 10));
+    // }
+
+    const double k_x = 1.0, k_y = 0.1;
+
+    void compute_rhs(int iter) {
         auto& rhs = u;
 
         zero(rhs);
@@ -110,13 +131,19 @@ private:
                     double gradient_prod = k_x * u.dx * v.dx + k_y * u.dy * v.dy;
                     double h = e2h(e[1]);
                     auto p = point(e, q);
-                    double val = u.val * v.val - steps.dt * gradient_prod + steps.dt * (dTy(h) + cannon(p[0], p[1], t)) * u.dy * v.val + steps.dt * f(h) * v.val;
+
+                    int t_begin = 1000 + 2000 * ((iter - 1000) / 2000);
+                    int t_end = 2000 + 2000 * ((iter - 2000) / 2000);
+
+                    double val =
+                        u.val * v.val - steps.dt * gradient_prod
+                        + steps.dt * (dTy(h) + cannon(p[0], p[1], iter, t_begin, t_end)) * u.dy * v.val
+                        + steps.dt * f(h) * v.val;
                     rhs(a[0], a[1]) += val * w * J;
                 }
             }
         }
     }
-
 };
 
 }  // namespace ads::problems
